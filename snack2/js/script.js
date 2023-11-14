68bdc00f44d4cca73ed95a43f05fc0fa34ccc3ae
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-14 12:13:46 +0100

    fix

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -4,10 +4,10 @@ for(let i = 0; i<10; i++){
     let newElement = document.createElement("p");
     newElement.textContent = i + 1;
     if( (i+1)%2 === 0){
-        newElement.style.backgroundColor = "green"
+        newElement.style.backgroundColor = "green";
     }
     else{
-        newElement.style.backgroundColor = "yellow"
+        newElement.style.backgroundColor = "yellow";
     }
-    taget.appendChild(newElement)
+    taget.appendChild(newElement);
 }
