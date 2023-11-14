c54a4e19d87552863b50595329d846bf98ea856a
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-14 12:23:04 +0100

    secondo fix

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,6 +1,6 @@
 let taget = document.querySelector("body");
 
-for(let i = 0; i<10; i++){
+for(let i = 0; i<6; i++){
     let newElement = document.createElement("p");
     newElement.textContent = i + 1;
     if( (i+1)%2 === 0){
