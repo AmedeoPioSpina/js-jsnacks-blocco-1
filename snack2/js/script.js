345f5eb61cc607231f6d1cb6c01f8d06b638c164
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-14 12:12:54 +0100

    finito

---
+++ snack2/js/script.js
@@ -0,0 +1,13 @@
+let taget = document.querySelector("body");
+
+for(let i = 0; i<10; i++){
+    let newElement = document.createElement("p");
+    newElement.textContent = i + 1;
+    if( (i+1)%2 === 0){
+        newElement.style.backgroundColor = "green"
+    }
+    else{
+        newElement.style.backgroundColor = "yellow"
+    }
+    taget.appendChild(newElement)
+}
