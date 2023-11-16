d034814343adae43e031cc37bf7b165326fcad34
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-16 12:32:07 +0100

    commit

---
+++ snack-5/js/script.js
@@ -0,0 +1,20 @@
+let inputList = document.querySelectorAll("input");
+let btn = document.querySelector("btn");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault;
+
+    let newElement = document.createElement("p");
+
+    if(inputList[0].value.length > inputList[1].value.length){
+        newElement.textContent = inputList[0].value.length + " è la parola più lunga";
+    }
+    else if(inputList[0].value.length < inputList[1].value.length){
+        newElement.textContent = inputList[1].value.length + " è la parola più lunga";
+    }
+    else{
+        newElement.textContent = "Sono della stessa lunghezza";
+    }
+    let body = document.querySelector("body");
+    body.appendChild(newElement);
+});
