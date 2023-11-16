ba7e0948519f9c06a1c875bd7d5b4e9557d4bb4c
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-16 12:36:07 +0100

    terzo fix

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -1,10 +1,9 @@
 let inputList = document.querySelectorAll("input");
 let btn = document.querySelector("button");
+let newElement = document.createElement("p");
 
 btn.addEventListener("click", (e) => {
     e.preventDefault;
-
-    let newElement = document.createElement("p");
 
     if(inputList[0].value.length > inputList[1].value.length){
         newElement.textContent = inputList[0].value + " è la parola più lunga";
